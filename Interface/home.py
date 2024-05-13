f4a7600756e394623982e72a308586a246c8d045
jdg0812 <73633120+jdg0812@users.noreply.github.com>
2024-05-13 23:32:16 +1000

    reads data

--- Interface/home.py
+++ Interface/home.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template, request
 from google.cloud import bigquery
+import os 
 
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "../Other + Old Scripts/data-systems-assignment-a8059c08d52e.json"
+
+client = bigquery.Client()
 
 
 app = Flask(__name__)
-
-# client = bigquery.Client()
 
 
 @app.route('/')
 def index():
 
-    # query = """
-    #     SELECT * FROM 'insert table name here'
-    # """
-    # query_job = client.query(query)
-    # results = query_job.result()
-
-    return(render_template('index.html'))
+    query = """ SELECT DISTINCT Flight_ID, Flight_Status, Destination_Airport, Departure_Airport, Departure_Time, Arrival_Time
+ FROM `data-systems-assignment.Airport_Dataset.Flights_Table_Test` 
+"""
+    query_job = client.query(query)
+    return(render_template('index.html', data=query_job.result()))
 
 @app.route('/login')
 def login():
