20c3bdb3b6606d0e299f2dde4af0da346392ad3e
rhogan284 <ryanhogan284@gmail.com>
2024-05-15 19:16:36 +1000

    Added Check-In Page

--- Interface/home.py
+++ Interface/home.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import mysql.connector
 import json
 
-# Load configuration from the JSON file
+# Load configuration from the config file
 config_path = "../config.json"
 with open(config_path, 'r') as config_file:
     config = json.load(config_file)
@@ -15,6 +15,7 @@ db_config = {
 }
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -33,6 +34,7 @@ def index():
 
     return render_template('index.html', data=data)
 
+
 @app.route('/refresh')
 def refresh():
     conn = mysql.connector.connect(**db_config)
@@ -50,18 +52,46 @@ def refresh():
 
     return jsonify(data)
 
+
 @app.route('/login')
 def login():
     return render_template('login.html')
+
 
 @app.route('/user_home', methods=['POST'])
 def user_home():
     username = request.form['username']
     return render_template('home.html', username=username)
 
+
 @app.route('/checkin')
 def checkin():
     return render_template('checkin.html')
+
+
+@app.route('/checkin/<flight_id>', methods=['GET'])
+def get_passenger_list(flight_id):
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+
+    query = f"""
+    SELECT Passport_No, Citizenship, Seat_No
+    FROM {config['ticket_table_name']}
+    WHERE Flight_ID = %s
+    """
+    cursor.execute(query, (flight_id,))
+    passengers = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    if passengers:
+        passenger_list = [
+            {"passportNumber": p[0], "citizenship": p[1], "seatNumber": p[2]}
+            for p in passengers]
+        return jsonify(success=True, passengers=passenger_list)
+    else:
+        return jsonify(success=False)
 
 
 if __name__ == '__main__':
