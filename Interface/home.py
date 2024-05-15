db09d7e9da559aea3f12c7cbf546e8fe26a709fa
rhogan284 <ryanhogan284@gmail.com>
2024-05-15 16:13:21 +1000

    Switched Data Input To MySQL Server

--- Interface/home.py
+++ Interface/home.py
@@ -1,37 +1,59 @@
 from flask import Flask, render_template, request
-from google.cloud import bigquery
-import os 
+import os
+import json
+import mysql.connector
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "../Other + Old Scripts/data-systems-assignment-a8059c08d52e.json"
 
-client = bigquery.Client()
-
+with open('../config.json', 'r') as config_file:
+    config = json.load(config_file)
 
 app = Flask(__name__)
+
+db_config = {
+    'user': config['db_username'],
+    'password': config['db_password'],
+    'host': config['jdbc_url'].split('//')[1].split(':')[0],
+    'database': config['jdbc_url'].split('/')[-1]
+}
 
 
 @app.route('/')
 def index():
+    # Establish MySQL connection
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
 
-    query = """ SELECT DISTINCT Flight_ID, Flight_Status, Destination_Airport, Departure_Airport, Departure_Time, Arrival_Time
- FROM `data-systems-assignment.Airport_Dataset.Flights_Table_Test` 
-"""
-    query_job = client.query(query)
-    return(render_template('index.html', data=query_job.result()))
+    query = """
+    SELECT DISTINCT Flight_ID, Flight_Status, Destination_Airport, Departure_Airport, Departure_Time, Arrival_Time
+    FROM Flight
+    """
+    cursor.execute(query)
+    data = cursor.fetchall()
+
+    print(data)
+
+    # Close the connection
+    cursor.close()
+    conn.close()
+
+    return render_template('index.html', data=data)
+
 
 @app.route('/login')
 def login():
-    return(render_template('login.html'))
+    return render_template('login.html')
 
 
 @app.route('/user_home', methods=['POST'])
 def user_home():
     username = request.form['username']
-    return(render_template('home.html', username=username))
+    return render_template('home.html', username=username)
+
 
 @app.route('/checkin')
 def checkin():
-    return(render_template('checkin.html'))
+    return render_template('checkin.html')
 
 
 if __name__ == '__main__':
